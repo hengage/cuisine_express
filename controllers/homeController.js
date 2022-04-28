6dbb5337ca48f014889c32a9f96cbcdfa2ce7f10
hengage <tunecarterkillz@gmail.com>
2022-04-28 14:21:10 +0100

    Refactor: removed unneeded code from homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,27 +1,5 @@
 "use strict";
 
-const courses = [
-    {
-        title: "skin care smoothies",
-        cost: 50
-    },
-    {
-        title: "white soup",
-        cost: 100
-    },
-    {
-        title: "afang soup",
-        cost: 100
-    },
-]
-
 exports.homePageController = (req, res) => {
     res.render('home');
-};
-
-
-exports.showCourseList = (req, res) => {
-    res.render('courses', {
-        coursesOffered: courses
-    });
 };
