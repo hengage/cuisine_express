d077515735f4e4c1fe8862b884d0eb4835472d48
hengage <tunecarterkillz@gmail.com>
2022-05-12 13:14:59 +0100

    Feature: used gmail for email provider

--- emailsender.js
+++ emailsender.js
@@ -3,25 +3,29 @@ const nodemailer = require('nodemailer');
 const makeReservationConfirmEmail = (currentUser, restaurant, date) => {
     
     const transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+        service: 'gmail',
         auth: {
-        user: process.env.NODEMAILER_AUTH_USER,
-        pass: process.env.NODEMAILER_AUTH_PASSWORD
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASSWORD
         }
     });
 
     const message = {
-        from: "Cuisine Express <noreply@cuisine_express.com>",
+        from: `Cuisine Express ${process.env.GMAIL_USER}`,
         to: currentUser.email,
         subject: "Table Reservation ",
-        html: `<div style="text-align: center;">
-        <h2>Your table reservation booking was successful</h2>
-        <p>You are getting this email because you booked a table at ${restaurant}.</p>
+        html: `
+        <div style="text-align: center;">
+            <h2>Your table reservation booking was successful</h2>
+            <p>You are getting this email because you booked a table at ${restaurant}.</p>
 
-        <p>Reservation Time: ${date}</p>
+            <p>Reservation Time: ${date}</p>
 
-        <p>Please endeavour to be present for your dining.</p>
+            <p>Please endeavour to be present for your dining.</p>
+
+            <div style="margin-top: 2em;">
+                <a href="https://cuisine-express.herokuapp.com/">cuisine-express.herokuapp.com</a>
+            </div>
         </div>`
     }
 
